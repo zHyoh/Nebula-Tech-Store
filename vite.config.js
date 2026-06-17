474305daae12084ef5d471ddcb4128070d01f3eb
Miguel <mpl1297@hotmail.com>
2026-06-16 20:01:26 -0500

    fix url base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Nebula-Tech-Store/', // <-- Agrega esta línea con el nombre exacto de tu repositorio de GitHub
 })
